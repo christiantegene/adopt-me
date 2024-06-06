bbe430fc6116f7e887b042c266e2e719b9e5c6a8
christiantegene <christiantegene@gmail.com>
2024-06-05 21:05:47 -0400

    Added picture Carousel for Details pages using a Class component

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
+import Carousel from "./Carousel";
 
 const Details = () => {
   const { id } = useParams();
@@ -21,8 +22,9 @@ const Details = () => {
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
-        <button>Adopt {pet.name}</button>
+        <Carousel images={pet.images} />
         <p>{pet.description}</p>
+        <button>Adopt {pet.name}</button>
       </div>
     </div>
   );
