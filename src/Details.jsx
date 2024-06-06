98a6e1ea09822fe94dc47b5047de0b807d800c4a
christiantegene <christiantegene@gmail.com>
2024-06-06 19:42:12 -0400

    Added modal for Adopt Pet button on Details page using Portals

--- src/Details.jsx
+++ src/Details.jsx
@@ -3,10 +3,13 @@ import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import { useState } from "react";
+import Modal from "./Modal";
 
 const Details = () => {
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
+  const [showModal, setShowModal] = useState(false);
 
   if (results.isLoading) {
     return (
@@ -25,7 +28,20 @@ const Details = () => {
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
         <Carousel images={pet.images} />
         <p>{pet.description}</p>
-        <button>Adopt {pet.name}</button>
+        <button onClick={() => setShowModal(true)}>
+          Adopt {pet.name}
+        </button>{" "}
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {pet.name}?</h1>
+              <div className="buttons">
+                <button>Yes</button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
